0c422860191abadbf01092870c05d3920582b5a7
GhostMobin <s1810677110@ru.ac.bd>
2023-07-28 12:45:06 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'brand-primary': '#B68C5A',
+                'headline': '#414040',
+                'text': '#707070',
+            },
+        },
+    },
+    plugins: [],
+}
